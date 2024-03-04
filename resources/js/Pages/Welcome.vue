f33ae4c4341a7bd9a5b2b1c989f4969441f8d341
josu1m <mendozam6991m@gmail.com>
2024-03-04 03:10:37 -0500

    5 subida

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -14,13 +14,8 @@ defineProps({
     <Head title="Welcome" />
 
     <div>
-        <Link :href="route('login')"
-            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-        Log in</Link>
 
-        <Link v-if="canRegister" :href="route('register')"
-            class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-        Register</Link>
+        <Link href="/login">Inicio</Link>
 
     </div>
 </template>
