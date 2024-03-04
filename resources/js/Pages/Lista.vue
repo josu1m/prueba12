a0513cd25ae2c25c1e22ef4d537e58509823bafc
josu1m <mendozam6991m@gmail.com>
2024-03-04 02:55:52 -0500

    3 subida

---
+++ resources/js/Pages/Lista.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Lista</h1>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,3 +34,4 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+Route::resource("lista",ListaController::class);
